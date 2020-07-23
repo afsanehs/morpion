e66cdb822ed5f0a36dd8a721dbfd7c5cba22f29d
afsanehs <63603159+afsanehs@users.noreply.github.com>
2020-07-23 23:49:29 +0200

    Update board_case.rb

--- lib/board_case.rb
+++ lib/board_case.rb
@@ -1,8 +1,11 @@
 class BoardCase
   attr_accessor :state
+  #initialize the state with a random string, starting with a known string
+  #it was done this way to make it easier to do the winner check without having to also check everything for nil 
   def initialize
     @state = "default-#{rand}"
   end
+  #provides a textual, colorized representation of the case's state
   def print_state
     case @state
     when "X"
@@ -13,9 +16,11 @@ class BoardCase
       " "
     end
   end
+  #returns a boolean indicating if the case has already been played 
   def played?
     !(@state =~ /default/)
   end
+  #mark the case with x or o
   def take(mark)
     @state = mark
   end
